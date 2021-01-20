a4885fa4cd9e1c57a97e6f80b461ada194dddfd9
aidan <aidanguarniere@gmail.com>
2021-01-19 19:11:03 -0800

    .env file and Sequelize initiation

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// import Sequelize constructor 
+const Sequelize = require('sequelize');
+
+// create connection database, pass MYSQL info for use
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql', 
+    port: 3306
+});
+
+module.exports = sequelize;
