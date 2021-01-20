5ca2590d5fa76faeae7026248ba530b56d4d62ce
aidan <aidanguarniere@gmail.com>
2021-01-20 12:41:00 -0800

    created scaleable CRUD routes for User model

---
+++ server.js
@@ -0,0 +1,18 @@
+// requirements
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// activate routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'))
+});
